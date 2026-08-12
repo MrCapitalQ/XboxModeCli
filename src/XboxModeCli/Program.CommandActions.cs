a12a5a16cd8ac96ee7b970717ed8dbe420b48539
MrCapitalQ <66032879+MrCapitalQ@users.noreply.github.com>
2026-08-12 14:23:00 -0400

    Cancel previous commands on new invocations. (#10)
    For potentially long running commands like activating/deactiving XBOX mode where waits and delays are involved, introduce a cancellation mechanism where new commands from other instances trigger a cancellation on current instances. This is done by watching a shared temp file to track the "current" instance.

--- src/XboxModeCli/Program.CommandActions.cs
+++ src/XboxModeCli/Program.CommandActions.cs
@@ -10,6 +10,8 @@ internal static class CommandActions
         bool isActive,
         CancellationToken cancellationToken)
     {
+        cancellationToken = await CreateMultiInstanceCancellationAsync(cancellationToken);
+
         Console.WriteLine($"{(isActive ? "Activating" : "Deactivating")} XBOX mode...");
 
         if (isActive == await XboxModeHelper.IsXboxModeAsync(cancellationToken))
@@ -45,39 +47,48 @@ internal static class CommandActions
         {
             var movePointerDelay = TimeSpan.FromSeconds(movePointerDelaySeconds);
             postCommandTasks.Add(ExecuteActionWithDelayAsync(() =>
-            {
-                Console.WriteLine("Moving mouse pointer offscreen.");
-                if (XboxModeHelper.GetXboxModeInfoAsync(cancellationToken).Result is { } xboxModeInfo)
                 {
-                    PInvoke.SetCursorPos(xboxModeInfo.MonitorSize.Width, xboxModeInfo.MonitorSize.Height);
-                }
-            }, movePointerDelay, $"Waiting {movePointerDelay} before moving mouse pointer offscreen."));
+                    Console.WriteLine("Moving mouse pointer offscreen.");
+                    if (XboxModeHelper.GetXboxModeInfoAsync(cancellationToken).Result is { } xboxModeInfo)
+                    {
+                        PInvoke.SetCursorPos(xboxModeInfo.MonitorSize.Width, xboxModeInfo.MonitorSize.Height);
+                    }
+                },
+                movePointerDelay,
+                $"Waiting {movePointerDelay} before moving mouse pointer offscreen.",
+                cancellationToken));
         }
 
         if (!isActive && parseResult.GetValue(CommandOptions.ExitXboxAppOption) is { } exitXboxAppDelayMs)
         {
             var exitXboxAppDelay = TimeSpan.FromSeconds(exitXboxAppDelayMs);
             postCommandTasks.Add(ExecuteActionWithDelayAsync(() =>
-            {
-                Console.WriteLine("Closing the XBOX app...");
-                foreach (var process in Process.GetProcessesByName("XboxPcApp"))
                 {
-                    process.CloseMainWindow();
-                }
-            }, exitXboxAppDelay, $"Waiting {exitXboxAppDelay} before closing the XBOX app."));
+                    Console.WriteLine("Closing the XBOX app...");
+                    foreach (var process in Process.GetProcessesByName("XboxPcApp"))
+                    {
+                        process.CloseMainWindow();
+                    }
+                },
+                exitXboxAppDelay,
+                $"Waiting {exitXboxAppDelay} before closing the XBOX app.",
+                cancellationToken));
         }
 
         if (parseResult.GetValue(CommandOptions.CloseSettingsAppOption) is { } exitSettingsAppDelaySeconds)
         {
             var exitSettingsAppDelay = TimeSpan.FromSeconds(exitSettingsAppDelaySeconds);
             postCommandTasks.Add(ExecuteActionWithDelayAsync(() =>
-            {
-                Console.WriteLine("Closing the Settings app...");
-                foreach (var process in Process.GetProcessesByName("SystemSettings"))
                 {
-                    process.Kill();
-                }
-            }, exitSettingsAppDelay, $"Waiting {exitSettingsAppDelay} before closing the Settings app."));
+                    Console.WriteLine("Closing the Settings app...");
+                    foreach (var process in Process.GetProcessesByName("SystemSettings"))
+                    {
+                        process.Kill();
+                    }
+                },
+                exitSettingsAppDelay,
+                $"Waiting {exitSettingsAppDelay} before closing the Settings app.",
+                cancellationToken));
         }
 
         await Task.WhenAll(postCommandTasks);
@@ -85,14 +96,52 @@ internal static class CommandActions
         return 0;
     }
 
-    private static async Task ExecuteActionWithDelayAsync(Action action, TimeSpan delay, string delayMessage)
+    private static async Task ExecuteActionWithDelayAsync(Action action,
+        TimeSpan delay,
+        string delayMessage,
+        CancellationToken cancellationToken = default)
     {
         if (delay > TimeSpan.Zero)
         {
             Console.WriteLine(delayMessage);
-            await Task.Delay(delay);
+            await Task.Delay(delay, cancellationToken);
         }
 
         action();
+    }
+
+    private static async Task<CancellationToken> CreateMultiInstanceCancellationAsync(CancellationToken originalCancellationToken = default)
+    {
+        try
+        {
+            var fileName = "XboxModeCli.txt";
+            var path = Path.Combine(Path.GetTempPath(), fileName);
+
+            var currentId = Guid.NewGuid().ToString();
+            var cts = CancellationTokenSource.CreateLinkedTokenSource(originalCancellationToken);
+
+            var watcher = new FileSystemWatcher(Path.GetTempPath(), fileName);
+            watcher.Changed += async (s, e) =>
+            {
+                if (currentId == await File.ReadAllTextAsync(path, originalCancellationToken))
+                    return;
+
+                Console.WriteLine("Another instance has started execution. Cancelling current execution.");
+                cts.Cancel();
+                watcher?.Dispose();
+                watcher = null;
+            };
+
+            await File.WriteAllTextAsync(path, currentId, originalCancellationToken);
+
+            watcher.EnableRaisingEvents = true;
+
+            return cts.Token;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Failed to ensure single instance execution and cannot continue.");
+            throw;
+        }
     }
 }
